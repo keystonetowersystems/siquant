1f174e38483515f0e6ec4f5bc6dd26b044f6eb15
Greg Echelberger <greg@keystonetowersystems.com>
2018-10-10 12:17:14 -0600

    updated setup metadata

--- setup.py
+++ setup.py
@@ -8,6 +8,18 @@ def readme():
     with open(filepath) as f:
         return f.read()
 
+PROJECT_URLS = {
+    "Documentation": "https://siquant.readthedocs.io/",
+    "Bug Tracker": "https://github.com/keystonetowersystems/siquant/issues",
+    "Source Code": "https://github.com/keystonetowersystems/siquant",
+}
+KEYWORDS = [
+    "SI",
+    "units",
+    "dimensional analysis",
+    "quantities"
+]
+
 
 INSTALL_REQUIRES = []
 EXTRAS_REQUIRE = {"docs": ["sphinx", "numpy"], "tests": ["coverage", "pytest", "numpy"]}
@@ -16,12 +28,15 @@ EXTRAS_REQUIRE["dev"] = (
 )
 
 
+
 setup(
     name="siquant",
     version="4.0.0b5",
     description="Dimensional Analysis and Unit Tracking Library",
     long_description=readme(),
     url="https://github.com/keystonetowersystems/siquant",
+    project_urls=PROJECT_URLS,
+    keywords=KEYWORDS,
     author="Greg Echelberger",
     author_email="greg@keystonetowersystems.com",
     packages=find_packages(exclude=("tests",)),
