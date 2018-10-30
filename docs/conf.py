2cdc890501dc7c8b5e61b6e384cd02cccf389f41
Greg Echelberger <greg@keystonetowersystems.com>
2018-10-30 17:06:43 -0600

    Bump version: 4.0.0b8 → 4.0.0b9

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Greg Echelberger"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "4.0.0b8"
+release = "4.0.0b9"
 
 
 # -- General configuration ---------------------------------------------------
